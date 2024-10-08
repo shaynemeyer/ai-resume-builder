39b8fe22461e67f5698fcfe82158146983c9998a
Shayne Meyer <shayne.meyer@gmail.com>
2024-10-08 07:00:15 -0700

    added authentication with clerk

--- components/nav/TopNav.tsx
+++ components/nav/TopNav.tsx
@@ -1,14 +1,36 @@
+"use client";
+
 import Link from "next/link";
 import { ModeToggle } from "./ModeToggle";
 import Image from "next/image";
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
 
 function TopNav() {
+  const { isSignedIn, user } = useUser();
   return (
-    <nav className="flex justify-between py-1 mx-2">
+    <nav className="flex justify-between p-1 shadow">
       <Link href="/">
         <Image src="/images/logo.svg" height={50} width={50} alt="Logo" />
       </Link>
-      <ModeToggle />
+      <div className="flex justify-end items-center gap-2">
+        {isSignedIn && (
+          <Link href="/dashboard">{user?.fullName}&apos;s Dashboard</Link>
+        )}
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+
+        <ModeToggle />
+      </div>
     </nav>
   );
 }
